acc68a624cf785b456b5cba0ee537cbc5eb6b902
sahad <msahadpp@gmail.com>
2023-06-01 11:19:26 +0530

    some updations

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,3 +1,3 @@
-// const baseUrl = 'https://tummoc.onrender.com';
-const baseUrl = 'http://localhost:8080'
+const baseUrl = 'https://dashboard-backend-gki6.onrender.com';
+// const baseUrl = 'http://localhost:8080'
 export default baseUrl;
